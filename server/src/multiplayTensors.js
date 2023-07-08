a726d8912aabd470eb3f3cc3cfa6433d3cb38cd4
vitalspace <myglobalsounds@gmail.com>
2023-07-08 15:12:26 -0600

    multiply 2 bidirectional tensors example

---
+++ server/src/multiplayTensors.js
@@ -0,0 +1,9 @@
+import * as tf from "@tensorflow/tfjs";
+
+
+// This example uses "matMul" to multiply 2 bidirectional tensors
+const tfa = tf.tensor2d([ [1, 2, -3], [4, 0, -2]]);
+const tfb = tf.tensor2d([[3,1], [2, 4] , [-1, 5]]);
+const result = tf.matMul(tfa, tfb);
+
+result.print();
